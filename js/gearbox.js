be5d0ffd37be224eb91b84bebd8164771c506ad6
Lucas Oliveira <lucas.oliveira@rbstv.com.br>
2016-07-18 12:11:20 -0300

    CSS Animate

--- js/gearbox.js
+++ js/gearbox.js
@@ -7,3 +7,30 @@
             }, 1000);
         }
     });
+
+
+
+$(window).scroll(function() {
+
+  var orgElementPos = $('#extra').offset();
+  orgElementTop = orgElementPos.top;
+
+  if ($(window).scrollTop() >= (orgElementTop)) {
+
+    $('#menu').css('top','.8em');
+    $('.logo').css('height','1.5em');
+    $('.logo').css('margin-top','-.3em');
+       $('#header-wrapper').css('height','4em');
+       $('#header-wrapper').css('background','#225DAE');
+      $('#header-wrapper').css('	box-shadow','0px 2px 10px ');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+
+    $('#menu').css('top','2em');
+          $('.logo').css('height','2em');
+          $('#header-wrapper').css('height','133');
+        $('#header-wrapper').css('background','');
+        $('#header-wrapper').css('	box-shadow','');
+
+  }
+});
